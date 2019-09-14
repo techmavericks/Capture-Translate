f28bc1777b03822cb5a1dab2ef66a8ac9b66f8ee
vandan bhingradiya <50577405+vandan103@users.noreply.github.com>
2019-09-14 11:10:43 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.traslator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button b1=(Button)findViewById(R.id.b1);
+        Button b2=(Button)findViewById(R.id.b2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Main3Activity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
